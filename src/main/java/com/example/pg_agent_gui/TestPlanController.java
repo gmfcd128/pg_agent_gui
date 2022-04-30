31b154effbe0c3ae042f188324680b6ada4d977e
林立軒 <gmfcd128@gmail.com>
2022-04-30 09:48:53 +0800

    backcup

--- src/main/java/com/example/pg_agent_gui/TestPlanController.java
+++ src/main/java/com/example/pg_agent_gui/TestPlanController.java
@@ -1,11 +1,14 @@
 package com.example.pg_agent_gui;
 
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import model.PGConfigDelta;
 import model.ValueType;
 
@@ -17,12 +20,26 @@ public class TestPlanController {
     private Pane configDeltaPanel;
 
 
-    HashMap<PGConfigDelta, FlowPane> area;
+    HashMap<PGConfigDelta, VBox> area;
 
     public void initialize() {
         area = new HashMap<>();
         PGConfigDelta test = new PGConfigDelta("work_mem", "16384", "kb", "integer");
+        VBox shit = new VBox();
+        HBox header = new HBox();
+        header.getChildren().add(new Label("work_mem"));
+
         FlowPane fuck = new FlowPane();
+        Button deleteAreaButton = new Button("移除");
+        deleteAreaButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                area.get("work_mem").getChildren().clear();
+                area.remove("work_mem");
+            }
+        });
+        header.getChildren().add(deleteAreaButton);
+
         fuck.setHgap(10);
         fuck.prefWidthProperty().bind(configDeltaPanel.widthProperty());
         Button addFieldButton = new Button("+");
@@ -31,11 +48,15 @@ public class TestPlanController {
         deleteFieldButton.setOnMouseClicked(event -> fuck.getChildren().remove(fuck.getChildren().size() - 2));
         fuck.getChildren().add(addFieldButton);
         fuck.getChildren().add(deleteFieldButton);
-        area.put(test, fuck);
+        shit.getChildren().add(header);
+        shit.getChildren().add(fuck);
+        area.put(test, shit);
         configDeltaPanel.getChildren().add(new Label(test.getName()));
         configDeltaPanel.getChildren().add(fuck);
         configDeltaPanel.getChildren().add(new Separator());
     }
+
+
 
 
 }
